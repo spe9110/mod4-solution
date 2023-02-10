25612af328ecd1f8195aec9a42fa9cd47e2cf792
spe9110 <117994537+spe9110@users.noreply.github.com>
2023-02-10 01:24:28 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+var greet = function (name) {
+    if (name[0].toLowerCase() === 'j') {
+        console.log("Goodbye " + name + "!");
+    } else {
+        console.log("Hello " + name + "!");
+    }
+};
+
+greet("John");
+
+greet("jun");
+
+greet("Spencer");
+
